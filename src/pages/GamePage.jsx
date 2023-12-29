3b3198e15f9ce600db53bb1271281bfb96446072
DrewAMSD <drewalgismiller2@gmail.com>
2023-12-29 00:07:17 -0800

    cells, grid, and start/stop game buttons, missing event while game is running

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,10 +1,104 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./GamePage.css";
 
-function Game() {
+function Cell(props) {
+  const [statusCell, setStatusCell] = useState("dead");
+
+  function handleClick() {
+    if (!props.gameIsRunning) {
+      setStatusCell(statusCell === "dead" ? "alive" : "dead");
+    }
+  }
+
   return (
     <>
-      <div>Game Page</div>
+      <div
+        className={`cell ${
+          statusCell === "alive" ? "cell-alive" : "cell-dead"
+        }`}
+        onClick={handleClick}
+      ></div>
+    </>
+  );
+}
+
+function Grid(props) {
+  let grid = [];
+
+  function initializeGrid() {
+    for (let i = 0; i < 15; i++) {
+      let row = [];
+      for (let j = 0; j < 15; j++) {
+        row.push(Cell(props));
+      }
+      grid.push(row);
+    }
+  }
+  initializeGrid();
+
+  useEffect(() => {
+    let intervalId;
+
+    if (props.gameIsRunning) {
+      intervalId = setInterval(() => {
+        //write here each iteration of running the game
+      }, 1000); // time interval inbetween running effect
+    }
+
+    return () => {
+      // Cleanup function to clear the interval when the component unmounts or gameIsRunning becomes false
+      clearInterval(intervalId);
+    };
+  }, [props.gameIsRunning]);
+
+  return (
+    <>
+      <div className="grid">
+        {grid.map((row, i) => (
+          <div className="grid-row" key={i}>
+            {row.map((cell, j) => (
+              <div key={j}>{cell}</div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+function Game() {
+  const [gameIsRunning, setGameIsRunning] = useState(false);
+
+  const startGame = () => {
+    if (!gameIsRunning) {
+      setGameIsRunning(true);
+    }
+  };
+
+  const stopGame = () => {
+    if (gameIsRunning) {
+      setGameIsRunning(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="game-container">
+        <div className="grid-container">
+          <Grid gameIsRunning={gameIsRunning} />
+        </div>
+        <div className="game-buttons-container">
+          <button
+            className="game-buttons start-game-button"
+            onClick={startGame}
+          >
+            Start Game
+          </button>
+          <button className="game-buttons stop-game-button" onClick={stopGame}>
+            Stop Game
+          </button>
+        </div>
+      </div>
     </>
   );
 }
